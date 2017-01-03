557e46c48f78bf112ae2e66734256d776df9f369
devhwi <zziller03@gmail.com>
2017-01-03 19:08:25 +0900

    default settings

--- application/config/config.php
+++ application/config/config.php
@@ -76,7 +76,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'korean';
 
 /*
 |--------------------------------------------------------------------------
@@ -367,14 +367,18 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
+$config['sess_use_database'] = TRUE;
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
+$config['sess_table_name'] = 'ci_sessions';
+$config['sess_expire_on_close'] = TRUE;
+$config['sess_encrypt_cookie'] = FALSE;
+$config['sess_match_useragent'] = TRUE;
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
---
+++ application/views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Meta Informations -->
+    <meta http-equiv="Content-Type" context="text/html" charset="utf-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+
+    <!-- External CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css')?>">
+
+    <!-- Custom Style -->
+    <style type="text/css">
+
+    </style>
+
+    <!-- title -->
+    <title>Buras</title>
+  </head>
+  <body>
+    <!-- container-fluid start -->
+    <div class="container-fluid">
