2c35c4fc980bbbd1a84774e675f2a73f24e64ffa
devhwi <zziller03@gmail.com>
2017-01-16 01:27:08 +0900

    update welcome page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -61,11 +61,22 @@
 		<code>구축기간 : 2017.01.01 ~ 2017.01.31 (예상)</code>
 
 		<p>간단하게 진행상황을 볼 수 있습니다.</p>
-
 		<code><i class="fa fa-github" aria-hidden="true"></i>&nbsp;<a href="https://github.com/devhwi/CI_Buras">https://github.com/devhwi/CI_Buras</a></code>
 
-		<p>테스트 해보기</p>
+		<p>진행현황</p>
+		<code>
+		2017.01.02(월) : 요구사항 정리 및 플랜 작성<br>
+	  2017.01.03(화) : DB 설계<br>
+	  2017.01.04(수) : 화면 설계<br>
+	  2017.01.05(목) : DB 설계 완료<br>
+	  2017.01.09(월) : 로그인 화면 및 메인 화면<br>
+	  2017.01.10(화) : 회원가입<br>
+	  2017.01.11(수) : 의류 전체 보기 및 검색, 의류 상세 보기 및 품번별 대여 현황, 렌탈 등록<br>
+	  2017.01.12(목) : 나의 렌탈 현황 보기<br>
+	  2017.01.15(일) : 멤버 보기, 멤버 검색(이름, 기수)<br>
+		</code>
 
+		<p>테스트 해보기</p>
 		<code><a href="http://buras.cafe24.com/Login">로그인 페이지</a><br>
 					테스트 id : test / pw : test</code>
 	</div>
